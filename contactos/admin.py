a55c4076dcbe9279058a8dcf6624c7d26d3416b0
octavio gutierrez <octavio.gutierrez02@inacapmail.cl>
2026-09-15 13:26:56 -0300

    15-09-2026

--- contactos/admin.py
+++ contactos/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Contact
 
-admin.site.register(Contact)
 class contactAdmin(admin.ModelAdmin):
     list_display = ("nombre","precio","stock","activo")
     search_fields = ("nombre",)
