9d4fe4d9f10e179a17d1b4c50a705463c4335738
octavio gutierrez <octavio.gutierrez02@inacapmail.cl>
2026-09-21 13:17:04 -0300

    localhost

--- agenda_contactos/settings.py
+++ agenda_contactos/settings.py
@@ -16,7 +16,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-agenda-contactos-local"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'octaviogutierrez-eva1backend.vercel.app'
+    'localhost',
+    '127.0.0.1'
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
